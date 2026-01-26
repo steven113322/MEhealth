e5d65fee1336f790f3f7d76156b5e4e9b3a7a830
steven113322 <aoligei132168@gmail.com>
2026-01-26 10:24:09 -0800

    Initial Commit

---
+++ MEhealth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MEhealth
+//
+//  Created by Student on 1/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MEhealth/MEhealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  MEhealthApp.swift
+//  MEhealth
+//
+//  Created by Student on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct MEhealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
