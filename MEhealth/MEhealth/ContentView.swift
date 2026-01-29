24f657e6eb283c14a16db5b64d5b1c51d9104280
steven113322 <aoligei132168@gmail.com>
2026-01-29 10:21:52 -0800

    Add files via upload

---
+++ MEhealth/MEhealth/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  MEhealth
+//
+//  Created by Student on 1/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack{
+            Text("MEhealth")
+                .font(.largeTitle)
+                .position(x: 200, y: 250)
+            VStack {
+                NavigationLink(destination: DoctorView()) {
+                    Text("Appointment")
+                }
+                NavigationLink(destination: Information()) {
+                    Text("Information")
+                }
+            }
+            .position(x: 200, y: 5)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
