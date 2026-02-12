774765ebd1be64db72fd8c012be3d5330f6efa9c
Brandon <epic4gaming8@gmail.com>
2026-02-12 10:01:09 -0800

    dr david doctor

--- MEhealth/MEhealth/DoctorView.swift
+++ MEhealth/MEhealth/DoctorView.swift
@@ -10,12 +10,74 @@ import SwiftUI
 struct DoctorView: View {
     @State private var isAlert: Bool = false
     var body: some View {
-        Button("Hello, World!") {
-            isAlert=true
-        }
-        .alert("Alert", isPresented: $isAlert) {
-            Button("OK") { } }
+        VStack {
+            //Title("Available Doctors")
+            Button("Dr. David Doctor") {
+                isAlert=true
+            }
+            .foregroundColor(.white) // Text color
+            .padding(25) // Adds space around the text
+            .background(Color.purple) // Background color
+            .cornerRadius(30)
+            .alert("Available Hours: 8 AM - 7 PM\nEmail: drdavid@therapiee.com", isPresented: $isAlert) {
+                Button("OK") { } }
+            Button("Dr. Steven Wang") {
+                isAlert=true
+            }
+            .foregroundColor(.white) // Text color
+            .padding(25) // Adds space around the text
+            .background(Color.purple) // Background color
+            .cornerRadius(30)
+            .alert("Available Hours: 9 AM - 5 PM\nEmail: drwang@therapiee.com", isPresented: $isAlert) {
+                Button("OK") { } }
+            Button("Dr. Hwarang Cho") {
+                isAlert=true
+            }
+            .foregroundColor(.white) // Text color
+            .padding(25) // Adds space around the text
+            .background(Color.purple) // Background color
+            .cornerRadius(30)
+            .alert("Available Hours: 10 AM - 5 PM\nEmail: drcho@therapee.com", isPresented: $isAlert) {
+                Button("OK") { } }
+            Button("Dr. Jing Chen") {
+                isAlert=true
+            }
+            .foregroundColor(.white) // Text color
+            .padding(25) // Adds space around the text
+            .background(Color.purple) // Background color
+            .cornerRadius(30)
+            .alert("Available Hours: 12 PM - 7 AM\nEmail: drchen@therapee.com", isPresented: $isAlert) {
+                Button("OK") { } }
+            Button("Dr. Arjun Patel") {
+                isAlert=true
+            }
+            .foregroundColor(.white) // Text color
+            .padding(25) // Adds space around the text
+            .background(Color.purple) // Background color
+            .cornerRadius(30)
+            .alert("Available Hours: 11 AM - 6 PM\nEmail: drpateltherapee.com", isPresented: $isAlert) {
+                Button("OK") { } }
+            Button("Dr. Dylan Reyes") {
+                isAlert=true
+            }
+            .foregroundColor(.white) // Text color
+            .padding(25) // Adds space around the text
+            .background(Color.purple) // Background color
+            .cornerRadius(30)
+            .alert("Available Hours: 6 AM - 7 PM\nEmail: drreyes@therapee.com", isPresented: $isAlert) {
+                Button("OK") { } }
+            Button("Dr. Brian Bik") {
+                isAlert=true
+            }
+            .foregroundColor(.white) // Text color
+            .padding(25) // Adds space around the text
+            .background(Color.purple) // Background color
+            .cornerRadius(30)
+            .alert("Available Hours: 8 AM - 5 PM\nEmail: drbik@therapee.com", isPresented: $isAlert) {
+                Button("OK") { } }
+        }.navigationTitle("Available Doctors")
     }
+    
 }
 
 #Preview {
